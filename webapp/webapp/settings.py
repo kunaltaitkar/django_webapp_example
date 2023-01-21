ae5452ed639513729d660a4f3be0e4a6ef50fae3
Kunal  Taitkar <kunaltaitkar@Kunals-MacBook-Pro.local>
2023-01-21 15:00:24 +0530

    Setup mysql database as default db

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "webapp.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'app_db',
+        'USER': 'root',
+        'PASSWORD': 'P@ssw0rd',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
